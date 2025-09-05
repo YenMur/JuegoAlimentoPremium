aa8d8561378b818f5f065db9488e765746200f2d
Mar0921 <mariana.parra_acosta@uao.edu.co>
2025-09-05 12:11:54 -0500

    cambios
    Musica, boton mute, icono

--- JuegoAlimento/Assets/Music/AudioManager.cs
+++ JuegoAlimento/Assets/Music/AudioManager.cs
@@ -3,20 +3,60 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    private bool isMuted = false;
+    [Header("ASIGNAR MANUALMENTE")]
+    [SerializeField] private Button volumeButton;   
+    [SerializeField] private Sprite soundOnIcon;    
+    [SerializeField] private Sprite soundOffIcon;   
 
-    [SerializeField] private Image iconButton;
-    [SerializeField] private Sprite soundOnIcon;
-    [SerializeField] private Sprite soundOffIcon;
+    const string PREF_KEY = "muted";
+
+    static bool s_initialized;
+    static bool s_isMuted;
+
+    Image iconImage; 
+
+    void Awake()
+    {
+        if (volumeButton != null)
+        {
+            iconImage = volumeButton.targetGraphic as Image
+                     ?? volumeButton.GetComponent<Image>()
+                     ?? volumeButton.GetComponentInChildren<Image>(true);
+        }
+
+        if (!s_initialized)
+        {
+            s_isMuted = PlayerPrefs.GetInt(PREF_KEY, 0) == 1;
+            s_initialized = true;
+        }
+
+        ApplyMute(s_isMuted);
+        RefreshIcon(s_isMuted);
+    }
+
+    void OnEnable()
+    {
+        s_isMuted = PlayerPrefs.GetInt(PREF_KEY, 0) == 1;
+        ApplyMute(s_isMuted);
+        RefreshIcon(s_isMuted);
+    }
 
     public void ToggleMute()
     {
-        isMuted = !isMuted;
-        AudioListener.pause = isMuted;
+        s_isMuted = !s_isMuted;
+        PlayerPrefs.SetInt(PREF_KEY, s_isMuted ? 1 : 0);
+        ApplyMute(s_isMuted);
+        RefreshIcon(s_isMuted);
+    }
 
-        if(iconButton != null)
-        {
-            iconButton.sprite = isMuted ? soundOffIcon : soundOnIcon;
-        }
+    void ApplyMute(bool muted)
+    {
+        AudioListener.pause = muted; 
+    }
+
+    void RefreshIcon(bool muted)
+    {
+        if (iconImage != null && soundOnIcon != null && soundOffIcon != null)
+            iconImage.sprite = muted ? soundOffIcon : soundOnIcon;
     }
 }
--- JuegoAlimento/Assets/Script/Juego/GameController5.cs
+++ JuegoAlimento/Assets/Script/Juego/GameController5.cs
@@ -6,48 +6,47 @@ using UnityEngine;
 public class GameController5 : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI TMP_Puntos;
-
     [SerializeField] private TextMeshProUGUI TMP_Tiempo;
-
     [SerializeField] private GameObject PanelPerdio;
-
     [SerializeField] private GameObject PanelInicio;
 
+    [Header("Audio")]
     [SerializeField] private AudioSource musicaSource;
-
+    [SerializeField] private AudioSource sfxSource;
     [SerializeField] private AudioClip musicaSad;
-
     [SerializeField] private AudioClip musicaWin;
+    [SerializeField] private AudioClip sfxComidaMala;
 
-    private float cuentaRegresiva = 40f; // Tiempo inicial en segundos
+    private float cuentaRegresiva = 40f;
     private float currentTime;
     private bool cuentaRegresivaActiva = true;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentTime= cuentaRegresiva;
+        currentTime = cuentaRegresiva;
         PanelPerdio.SetActive(false);
         PanelInicio.SetActive(false);
+
+        if (sfxSource == null)
+        {
+            sfxSource = gameObject.AddComponent<AudioSource>();
+            sfxSource.playOnAwake = false;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        if (GameManager.Instance.juegoIniciado&&cuentaRegresivaActiva)
+        if (GameManager.Instance.juegoIniciado && cuentaRegresivaActiva)
         {
             currentTime -= Time.deltaTime;
-
-            if(currentTime > 0)
+            if (currentTime > 0)
             {
                 UpdateCountdownUI(currentTime);
             }
             else
             {
-                cuentaRegresivaActiva=false;
-                currentTime=0;
+                cuentaRegresivaActiva = false;
+                currentTime = 0;
                 UpdateCountdownUI(currentTime);
                 Ganar();
             }
@@ -57,59 +56,66 @@ public class GameController5 : MonoBehaviour
 
     void UpdateCountdownUI(float time)
     {
-        int minutes=Mathf.FloorToInt(time / 60);
-        int seconds=Mathf.FloorToInt(time % 60);
-        TMP_Tiempo.text=string.Format("{0:00}:{1:00}", minutes,seconds);
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+        TMP_Tiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     public void ActualizarPuntos()
     {
-        TMP_Puntos.text=GameManager.Instance.puntos.ToString("D3");
+        TMP_Puntos.text = GameManager.Instance.puntos.ToString("D3");
+    }
+
+    public void ReproducirSonidoComidaMala()
+    {
+        if (sfxComidaMala != null && sfxSource != null)
+        {
+            sfxSource.PlayOneShot(sfxComidaMala);
+        }
     }
 
     public void Perder()
     {
-        cuentaRegresivaActiva=false;
+        cuentaRegresivaActiva = false;
         PanelPerdio.SetActive(true);
-        GameManager.Instance.juegoTerminado=true;
-        GameManager.Instance.partidaGanada=false;
-
+        GameManager.Instance.juegoTerminado = true;
+        GameManager.Instance.partidaGanada = false;
         StartCoroutine(CargarScore());
 
-        musicaSource.Stop();
-
-        musicaSource.clip=musicaSad;
-        musicaSource.loop=false;
-        musicaSource.Play();
+        if (musicaSource != null && musicaSad != null)
+        {
+            musicaSource.Stop();
+            musicaSource.clip = musicaSad;
+            musicaSource.loop = false;
+            musicaSource.Play();
+        }
 
         GameManager.Instance.GuardarDatosJugador();
     }
 
     public void Ganar()
     {
-        cuentaRegresivaActiva=false;
+        cuentaRegresivaActiva = false;
         PanelInicio.SetActive(true);
-        GameManager.Instance.juegoTerminado=true;
-        GameManager.Instance.partidaGanada=true;
-
+        GameManager.Instance.juegoTerminado = true;
+        GameManager.Instance.partidaGanada = true;
         Debug.Log("Voy a Resultado con partidaGanada=" + GameManager.Instance.partidaGanada);
-
-
         StartCoroutine(CargarScore());
 
-
-        musicaSource.Stop();
-
-        musicaSource.clip = musicaWin;
-        musicaSource.loop = false;
-        musicaSource.Play();
+        if (musicaSource != null && musicaWin != null)
+        {
+            musicaSource.Stop();
+            musicaSource.clip = musicaWin;
+            musicaSource.loop = false;
+            musicaSource.Play();
+        }
 
         GameManager.Instance.GuardarDatosJugador();
     }
 
     public void IniciarCuentaRegresiva()
     {
-        cuentaRegresivaActiva=true;
+        cuentaRegresivaActiva = true;
     }
 
     private IEnumerator CargarScore()
@@ -117,5 +123,4 @@ public class GameController5 : MonoBehaviour
         yield return new WaitForSeconds(2f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("Resultado");
     }
-
 }
