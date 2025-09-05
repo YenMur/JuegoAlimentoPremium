b534d30e896eb35b64d32e150a650e1075384bb4
YenMur <yennifer.murillo@uao.edu.co>
2025-09-04 22:47:32 -0500

    cambios ligeros y arreglos

--- JuegoAlimento/Assets/Music/AudioManager.cs
+++ JuegoAlimento/Assets/Music/AudioManager.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     private bool isMuted = false;
 
+    [SerializeField] private Image iconButton;
+    [SerializeField] private Sprite soundOnIcon;
+    [SerializeField] private Sprite soundOffIcon;
+
     public void ToggleMute()
     {
         isMuted = !isMuted;
         AudioListener.pause = isMuted;
+
+        if(iconButton != null)
+        {
+            iconButton.sprite = isMuted ? soundOffIcon : soundOnIcon;
+        }
     }
 }
